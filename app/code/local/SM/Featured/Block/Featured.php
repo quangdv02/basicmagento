69b74de317b76e8751332824c522c548b163eef5
quangdv <quangdv@smartosc.com>
2015-06-24 18:49:50 +0700

    ok

--- app/code/local/SM/Featured/Block/Featured.php
+++ app/code/local/SM/Featured/Block/Featured.php
@@ -1,10 +1,13 @@
 <?php
 class SM_Featured_Block_Featured extends Mage_Core_Block_Template{
 
-    public function getCollection(){
+    public function getFeaturedProduct(){
+        $current_category = Mage::registry('current_category');
         $collection = Mage::getModel('catalog/product')->getCollection()
             ->addAttributeToSelect('*')
-            ->addAttributeToFilter('is_featured',1)->load();
+            ->addAttributeToFilter('is_featured',1);
+        if($current_category)
+            $collection->addCategoryFilter($current_category);
         return $collection;
     }
 }
---
+++ app/code/local/SM/Featured/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class SM_Featured_Helper_Data extends Mage_Core_Helper_Abstract{
+
+}
